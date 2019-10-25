c613ea3ae6500f5779998f4ef0bd127aad44fa07
tloinny <tloinny@gmail.com>
2019-10-25 11:44:30 +0800

    2019/10/25 11:4PM

--- LowerMachine/stm32/DCMotorNode/functions/motor_operate.c
+++ LowerMachine/stm32/DCMotorNode/functions/motor_operate.c
@@ -22,7 +22,7 @@ float wheel_speed_goal;	/* 轮子目标速度 */
 void motor_init(void)
 {
 	TIM4_PWM_Init(TIM4_ARR,TIM4_PSC);	/* 初始化TIM4,提供给电机驱动使用 */
-	as5600_init();	/* 初始化磁编码器 */
+	//as5600_init();	/* 初始化磁编码器 */
 	PID_CONTROLLER(0.035,0.01,0.02,25);	/* 初始化PID控制器，包括定时器3 */
 }
 
